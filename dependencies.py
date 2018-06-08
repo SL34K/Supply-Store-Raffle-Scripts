af3be78c43ad305056c5ca0da69b4a0eb070f3e5
SL34K <30938710+SL34K@users.noreply.github.com>
2018-06-08 10:59:25 +0100

    Add files via upload

---
+++ dependencies.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#.------..------..------..------..------.
+#|S.--. ||L.--. ||3.--. ||4.--. ||K.--. |
+#| :/\: || :/\: || :(): || :/\: || :/\: |
+#| :\/: || (__) || ()() || :\/: || :\/: |
+#| '--'S|| '--'L|| '--'3|| '--'4|| '--'K|
+#`------'`------'`------'`------'`------'
+#https://twitter.com/SL34K
+#https://github.com/SL34K
+#00110001 00110011
+#00110000 00110011
+#00110001 00111000 
+#########################################
+import pip
+pip.main(['install','bs4'])
+pip.main(['install','requests'])
+pip.main(['install','json'])
+pip.main(['install','datetime'])
+pip.main(['install','time'])
+pip.main(['install','random'])
+pip.main(['install','termcolor'])
+pip.main(['install','colorama'])
+pip.main(['install','names'])
+pip.main(['install','string'])
+pip.main(['install','imaplib'])
+pip.main(['install','re'])
